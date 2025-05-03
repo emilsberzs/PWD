d6a24956d27f3ee361dbd5bfdbbd4e4b106b00c2
Emils Berzs <emilsberzs@hotmail.com>
2025-05-03 09:00:05 +0100

    Few more NLTK operations

--- notes/ntlk_introduction.py
+++ notes/ntlk_introduction.py
@@ -1,4 +1,5 @@
 import nltk
+nltk.download('punkt_tab')
 from nltk.book import *
 
 # nltk.download('gutenberg')
@@ -9,22 +10,22 @@ from nltk.book import *
 # nltk.download('treebank')
 
 #First 100 words
-print(text1[0:100])
+#print(text1[0:100])
 
 #count occurrences
-print(text1.count('Dick'))
+#print(text1.count('Dick'))
 
 #length
-print(len(text1))
+#print(len(text1))
 
 #number of unique words
-print(len(set(text1)))
+#print(len(set(text1)))
 
 #find similiar words
-print(text1.similar('boat'))
+#print(text1.similar('boat'))
 
 #find context
-print(text1.concordance('boat'))
+#print(text1.concordance('boat'))
 
 
 test1 = 'ship'
@@ -35,9 +36,41 @@ count_text2 = text2.count(test1)
 
 count_text3 = text1.count(test2)
 count_text4 = text2.count(test2)
-print("Text 1: ",text1)
-print("Text 2: ",text2)
-print("\nTest 1 in Text 1:", count_text1)
-print("\nTest 1 in Text 2:", count_text2)
-print("\nTest 2 in Text 1:", count_text3)
-print("\nTest 2 in Text 2:", count_text4)
+#print("Text 1: ",text1)
+#print("Text 2: ",text2)
+#print("\nTest 1 in Text 1:", count_text1)
+#print("\nTest 1 in Text 2:", count_text2)
+#print("\nTest 2 in Text 1:", count_text3)
+#print("\nTest 2 in Text 2:", count_text4)
+
+
+
+
+'''Text as a list of words'''
+
+
+string1 = "This is a string"
+split_string = string1.split() #splits on whitespace (\n \r \t \f)
+#print(split_string)
+
+joined_split_string = ' '.join(split_string) #joins words in string with whitespace
+joined_split_string_dash = '-'.join(split_string) #joins words in string with dash '-'
+#print(joined_split_string)
+#print(joined_split_string_dash)
+
+#Prints all txt files as a list
+#print(nltk.corpus.gutenberg.fileids())
+
+#Stores the txt file in a variable
+textfile = nltk.corpus.gutenberg.raw('austen-emma.txt')
+#print(len(textfile))
+#print(textfile[0:100])
+
+sentences = nltk.sent_tokenize(textfile)
+#print(sentences[0])
+
+words = nltk.word_tokenize(sentences[0])
+#print(words)
+
+distribution = nltk.FreqDist(words).most_common # Shows frequency of word distribution
+print(distribution)
